9d4051322b01c14b8c0d672d3976a904c8d3c038
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-01-22 13:31:58 -0300

    Commit changes - 13:31 Brazil - 01/22/2022

--- Exercícios Curso Alura/exercicio3.py
+++ Exercícios Curso Alura/exercicio3.py
@@ -1,9 +1,33 @@
-total_de_tentativas = 3
-rodada = 1
+# Exercício de jogo de adivinhação - Alura
 
-while (rodada <= total_de_tentativas):
-    chute_str = input("Digite o seu número: ")
-    #resto omitido
+print("*" * 10)
+print("Bem vindo ao jogo de Adivinhação!")
+print("*" * 10)
+
+numero_secreto = 42
+total_de_tentativas = 3
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    # resto do código omitido
-    rodada = rodada + 1
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou " , chute_str)
+    chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou!")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+print("Fim do jogo")
