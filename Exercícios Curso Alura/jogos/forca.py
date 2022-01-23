0e1e0303e7ff322fe8a62b63d29ebaa5b2a909f8
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-01-22 21:58:03 -0300

    Commit changes - 21:58 Brazil - 01/22/2022

---
+++ Exercícios Curso Alura/jogos/forca.py
@@ -0,0 +1,19 @@
+# Exercício de jogo da forca - Alura
+
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+    
+    enforcou = False
+    acertou = False
+    
+    while not enforcou and not acertou:
+        print("jogando...")
+    
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
