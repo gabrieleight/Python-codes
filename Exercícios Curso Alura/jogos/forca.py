37319f914968f073261f3ad4cee014f254ad0f94
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-02-08 15:19:42 -0300

    Commit changes - 15:19 Brazil - 02/08/2022

--- Exercícios Curso Alura/jogos/forca.py
+++ Exercícios Curso Alura/jogos/forca.py
@@ -1,17 +1,28 @@
 # Exercício de jogo da forca - Alura
 
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo da Forca!***")
-    print("*********************************")
+    print("*" * 36)
+    print("   Bem vindo ao jogo da Forca!   ")
+    print("*" * 36)
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     
     enforcou = False
     acertou = False
     
+    print(letras_acertadas)
+    
     while not enforcou and not acertou:
-        print("jogando...")
+        chute = input("Chute uma letra: ").lower().strip()
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = chute
+                #print(f"Encontrei a letra '{chute}' na posição {index+1}")
+            index = index + 1
+    
+        print(letras_acertadas)
     
     print("Fim do jogo")
 
