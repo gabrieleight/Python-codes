6e075bb0c34236889a0eb27a91ad10e8c104b283
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-02-09 21:56:17 -0300

    Commit changes - 21:55 Brazil - 02/09/2022

--- Exercícios Curso Alura/jogos/forca.py
+++ Exercícios Curso Alura/jogos/forca.py
@@ -1,28 +1,46 @@
 # Exercício de jogo da forca - Alura
 
 def jogar():
-    print("*" * 36)
-    print("   Bem vindo ao jogo da Forca!   ")
-    print("*" * 36)
+    print("*" * 30)
+    print("Bem vindo ao jogo da Forca!")
+    print("*" * 30)
 
     palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    letras_acertadas = ["_" for letra in palavra_secreta]
     
     enforcou = False
     acertou = False
+    erros = 0
     
+    print()
     print(letras_acertadas)
+    print()
+    print("Você tem 6 tentativas para errar!")
+    print()
     
     while not enforcou and not acertou:
-        chute = input("Chute uma letra: ").lower().strip()
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = chute
-                #print(f"Encontrei a letra '{chute}' na posição {index+1}")
-            index = index + 1
+        chute = input("Chute uma letra: ").strip().lower()
+        
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra.upper():
+                    letras_acertadas[index] = chute
+                    #print(f"Encontrei a letra '{chute}' na posição {index+1}")
+                index += 1
+        else:
+            erros += 1
+            print(f"Você errou! Tentativas restantes: {6 - erros}")
+            
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
     
         print(letras_acertadas)
+    
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     
     print("Fim do jogo")
 
