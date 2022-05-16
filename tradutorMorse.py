772bec67959111df625eb2e8546ff666e01aa3e8
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-05-16 14:32:16 -0300

    Commit changes - 14:32 Brazil - 05/16/2022

--- tradutorMorse.py
+++ tradutorMorse.py
@@ -7,7 +7,8 @@ def IdentificaMorse(string):
 
 def ConverteMorse(string):
     simbolos = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E':'.', 'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---', 'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.',
-              'O':'---', 'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..', ' ': '/',
+              'O':'---', 'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-', 'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--', 'Z':'--..', ' ': '/', ',':'--..--',
+              '.':'.-.-.-', '?':'..--..', '!':'-.-.--', ':':'---...', ';':'-.-.-.', '(':'-.--.', ')':'-.--.-', 'Õ':'..--.', 'É':'..-..', '=':'-...-', 'Ã':'.-.-', 'Á':'.-.-',
               '1':'.----', '2':'..---', '3':'...--', '4':'....-', '5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.', '0':'-----'}
     stringMorse = IdentificaMorse(string)
     
@@ -38,6 +39,14 @@ def ConverteMorse(string):
 
 
 if __name__ == "__main__":
-    string = input("Digite a frase p/ converter em Morse (ou vice-versa): ").upper()
+    print('*' * 40)
+    print('*' * 7, 'Tradutor de Código Morse', '*' * 7)
+    print('*' * 6, 'Criado por Gabriel Gilberto', '*' * 5)
+    print('*' * 13, 'Versão 1.2.1', '*' * 13)
+    print('*' * 40)
+    print()
+    string = input("Digite uma comunicação curta p/ converter em Morse (ou vice-versa): \n").upper()
     stringMorse = ConverteMorse(string)
     print(stringMorse)
+    print()
+    print('*' * 14,'Thank you!', '*' * 14)
