875ddfe487182d865d1e66a7dec0bf4327bd6b80
Gabriel Gilberto Aguiar de Oliveira <gabrielgilberto@fisica.ufc.br>
2024-01-15 16:42:19 -0300

    Commit changes - 16:42 Brazil - 01/15/2024

--- extractorTweets.py
+++ extractorTweets.py
@@ -1,27 +1,53 @@
 import tweepy
 import pandas as pd
+from datetime import datetime
 
+def get_client(bearer_token):
+    # Autenticar no Twitter
+    client = tweepy.Client(bearer_token)
+    return client
 
-# Informar as credenciais de acesso entre aspas
-client = tweepy.Client(bearer_token="Inserir o bearer token aqui")
+def collecting_tweets(client, query):
+    tweets = client.search_all_tweets(query=query)
 
-# Definir a consulta de pesquisa
-query = "COLOCAR QUERY AQUI"
+    tweetsCollected = []
 
-# Criar uma lista para armazenar os tweets
-tweets = client.search_all_tweets(query=query)
-
-tweetsCollected = []
-
-for tweet in tweets.data:
-    nomeDeUsuario = tweet['user']['name']
-    dataEHora = tweet['created_at']
-    texto = tweet['text']
-    tweetsCollected.append([nomeDeUsuario, dataEHora, texto])
+    for tweet in tweets.data:
+        nomeDeUsuario = tweet['user']['name']
+        dataEHora = tweet['created_at']
+        texto = tweet['text']
+        tweetsCollected.append([nomeDeUsuario, dataEHora, texto])
     
+    return tweetsCollected
+    
+def exporting_tweets(tweetsCollected):
+    # Criar um DataFrame a partir da lista de tweets
+    df = pd.DataFrame(tweetsCollected, columns=['nomeDeUsuario', 'dataEHora', 'texto'])
 
-# Criar um DataFrame a partir da lista de tweets
-df = pd.DataFrame(tweetsCollected, columns=['nomeDeUsuario', 'dataEHora', 'texto'])
+    # Salvar o DataFrame em um arquivo CSV
+    df.to_csv(f"tweets{datetime.today}.csv", index=False)
+    print("Tweets exportados com sucesso!")
 
-# Salvar o DataFrame em um arquivo CSV
-df.to_csv('tweets2024-01-15.csv', index=False)
+###################################################################################################
+###################################################################################################
+#############VOCÊ PODE APENAS ALTERAR O CODIGO ABAIXO PARA TESTAR AS FUNCIONALIDADES###############
+
+def main():
+    # Token de acesso
+    bearer_token = "your_bearer_token_here"
+    
+    # Criar um cliente
+    client = get_client(bearer_token)
+    
+    # Query de busca
+    query = "your_query_here"
+    
+    # Coletar tweets
+    tweetsCollected = collecting_tweets(client, query)
+    
+    # Exportar tweets
+    exporting_tweets(tweetsCollected)
+    
+    
+if __name__ == "__main__":
+    main()
